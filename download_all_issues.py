edb0d59dcfde7e64bdf04b3ad40b610e36b12d6e
Awoldeyo <awoldeyo@hotmail.de>
2019-05-28 15:16:15 +0200

    Fixed errors: Drop duplicates keeps newest changes in final report.

--- download_all_issues.py
+++ download_all_issues.py
@@ -7,6 +7,7 @@ from pandas.api.types import CategoricalDtype
 from cocoa import Connection
 from format_df import JiraDf
 
+# Create timezone variable (UTC)
 tz = pytz.UTC
 
 # Connect to JIRA
@@ -35,7 +36,9 @@ draft.loc[:, 'from'] = pd.np.nan
 draft.loc[:, 'to'] = 'Draft'
 draft.loc[:, 'date'] = draft.loc[:, 'Created']
 dfDC = pd.concat((dfDC, draft), axis=0, ignore_index=True)
-dfDC = dfDC.sort_values(['key', 'date'], ascending=False)
+
+# Drop duplicated columns from dataframe
+dfDC = dfDC.loc[:, dfDC.columns.duplicated()==False]
 
 # Define status categories
 status = [
@@ -58,22 +61,37 @@ dfDC = dfDC.loc[status_filter, :]
 
 # Define important columns for final report
 important_cols = [
-        'key',
-        'Status',
-        'Created',
-        'date',
-        'from',
-        'to',
-        ]
+    'key',
+    'Department',
+    'Component/s',
+    'Status',
+    'Created',
+    'date',
+    'from',
+    'to',
+    ]
 
 # Keep important columns
 dfDC = dfDC.reindex(columns=important_cols)
+
+# Create a categorical dtype object based on status category
+cat_type = CategoricalDtype(categories=status,
+                            ordered=True)
+
+# Change columns "Status"/"to" to categorical dtypes
+dfDC.loc[:, 'Status'] = dfDC['Status'].astype(cat_type)
+dfDC.loc[:, 'from'] = dfDC['from'].astype(cat_type)
+dfDC.loc[:, 'to'] = dfDC['to'].astype(cat_type)
+
 
 # Apply date formatting to "Created"/"date" columns
 dfDC['Created'] = pd.to_datetime(dfDC['Created'])
 dfDC['date'] = pd.to_datetime(dfDC['date'])
 dfDC['Created'] = dfDC['Created'].dt.tz_localize(tz)
 dfDC['date'] = dfDC['date'].dt.tz_localize(tz)
+
+# Sort dataframe by key, date and from
+dfDC = dfDC.sort_values(['key', 'date', 'from'], ascending=False)
 
 # Create Timestamp for -14 days ago
 fourteen_days_ago = pd.datetime.now(tz) - Day(14)
@@ -84,29 +102,26 @@ date_range = (dfDC['date']<=fourteen_days_ago)
 # Create filtered dataframe
 dfDC_date_filtered = dfDC.loc[date_range,:].copy(True)
 
+# Store newly created (within 14 days) issues
+dfDC_new = dfDC.loc[(dfDC['Created']>=fourteen_days_ago), :].copy(True)
+
 # Transform date column values to string values
 dfDC_date_filtered.loc[:, 'date'] = dfDC_date_filtered['date'].map(
         lambda x: x.strftime('%d.%m.%Y')
         )
-
-# Sort filtered dataframe by key and date
-dfDC_date_filtered = dfDC_date_filtered.sort_values(
-        ['key', 'date'], 
-        ascending=False
+dfDC_new.loc[:, 'date'] = dfDC_new['date'].map(
+        lambda x: x.strftime('%d.%m.%Y')
         )
 
 # Keep latest change per date and key
 dfDC_date_filtered = dfDC_date_filtered.drop_duplicates(
-        subset=['key', 'date']
+        subset=['key']
+        )
+dfDC_new = dfDC_new.drop_duplicates(
+        subset=['key']
         )
 
-# Create a categorical dtype object based on status category
-cat_type = CategoricalDtype(categories=status,
-                            ordered=True)
-
-# Change columns "Status"/"to" to categorical dtypes
-dfDC_date_filtered.loc[:, 'Status'] = dfDC_date_filtered['Status'].astype(cat_type)
-dfDC_date_filtered.loc[:, 'to'] = dfDC_date_filtered['to'].astype(cat_type)
+dfDC_new.loc[:, 'Status change'] = 'neu'
 
 # Create filter for status change (i.e. compare current status with past status)
 no_changes = (dfDC_date_filtered['Status'] == dfDC_date_filtered['to'])
@@ -115,16 +130,28 @@ backward = (dfDC_date_filtered['Status'] < dfDC_date_filtered['to'])
 
 # Filter and set value in new column Status_change
 dfDC_date_filtered.loc[no_changes, 'Status change'] = 'unverändert'
-dfDC_date_filtered.loc[forward, 'Status change'] = 'neu'
+dfDC_date_filtered.loc[forward, 'Status change'] = 'vorgestuft'
 dfDC_date_filtered.loc[backward, 'Status change'] = 'zurückgestuft'
 
-# Transform created column values to string values
-dfDC_date_filtered['Created'] = dfDC_date_filtered['Created'].map(
-        lambda x: x.strftime('%d.%m.%Y')
-        )
 
-# Keep latest changes per key
-dfFinal = dfDC_date_filtered.drop_duplicates(subset=['key'])
+dfDC_total = pd.concat((dfDC_date_filtered, dfDC_new), axis=0, sort=False)
+
+
+dfFinal = dfDC_total.reindex(columns=['key', 'Department', 'Component/s', 'Status', 'Status change'])
+
+status_mapping = {
+    "Order approval": "2. Order approval",
+    "Resolved": "8. Resolved",
+    "Implemented": "5. Implemented",
+    "Draft": "1. Draft",
+    "Closed": "9. Closed",
+    "Concept Decision": "3. Concept Decision",
+    "Design Decision": "4. Design Decision",
+    "Concept legally approved": "6. Concept legally approved",
+    "Technical Implementation Started": "7. Technical Implementation Started",
+}
+
+dfFinal['Status'] = dfFinal.Status.map(status_mapping)
 
 # Export final dataframe to excel
 dfFinal.to_excel('All_issues_report.xlsx', sheet_name='Report', index=False)
